2ac1b361b3c0f55e35a1e860ab1c209bceeb2f9f
Miguel <miguevrgo@gmail.com>
2023-04-20 17:12:24 +0200

    Finished project

--- Language3/src/main.cpp
+++ Language3/src/main.cpp
@@ -48,34 +48,80 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
+    int begin_arg=1; //If -t max|min is not provided it starts in 1, else 3
+    bool max=false; //Default value for min|max is min (max=false)
+    
+    //Checks if arguments are valid and provided
+    if (argv[1][0]=='-'){
+        if (strcmp(argv[1],"-t")==0){
+            if (std::string(argv[2])=="max"){
+                max=true;
+                begin_arg=3;
+            }
+            else if(std::string(argv[2])=="min"){
+                begin_arg=3;
+            }
+            else{
+                showEnglishHelp(std::cerr);
+                return 1;
+            }
+
+        }
+        else{
+            showEnglishHelp(std::cerr);
+            return 1;
+        }
+        if (argc < 5 ) {
+            showEnglishHelp(std::cerr);
+            return 1;
+        }
+    }
+
     //Creates the language used as a reference
     Language ref_language;
-    ref_language.load(argv[1]);
+    ref_language.load(argv[begin_arg]);
    
     //Creates dynamic language array
     Language* languages=nullptr;
-    languages = new Language[argc-2];
+    languages = new Language[argc-begin_arg-1];
     
     //Loads each language into the array
-    for (int i=2;i<argc;i++){
-        languages[i-2].load(argv[i]);
+    for (int i=begin_arg+1;i<argc;i++){
+        languages[i-(begin_arg+1)].load(argv[i]);
     }
     
-    //Prints distance between texts and calculates nearest
-    double min_dist=100;
+    //String used to distinguish between max|min 
+    std::string max_min;
+    (max) ? max_min="Farthest" : max_min="Nearest";
+
+    //Prints distance between texts and calculates nearest|farthest distance
+    double min_max_dist;
+    (max) ? min_max_dist=0 : min_max_dist=100;
     double distance;
-    int min_index;
-    for (int i = 0; i < argc-2; i++) {
+    int min_max_index;
+    for (int i = 0; i < argc-begin_arg-1; i++) {
         distance = ref_language.getDistance(languages[i]);
-        if(distance < min_dist){
-            min_dist=distance;
-            min_index=i;
+        if (max){
+            if(distance > min_max_dist){
+                min_max_dist=distance;
+                min_max_index=i;
+            }  
         }
-        std::cout << "Distance to " << argv[i+2] << ": " << distance << std::endl;
+        else{
+            if(distance < min_max_dist){
+                min_max_dist=distance;
+                min_max_index=i;
+            }  
+        }
+        
+        std::cout << "Distance to " << argv[i+1+begin_arg] << ": " << distance << std::endl;
     }
 
-    std::cout << "Nearest language file: " << argv[min_index+2] << std::endl;
-    std::cout << "Identifier of the nearest language: " << languages[min_index].getLanguageId() << std::endl;
+    //Prints the nearest|farthest language file
+    std::cout << max_min <<" language file: " << argv[min_max_index+1+begin_arg] << std::endl;
+    (max) ? max_min="farthest" : max_min="nearest";
+    std::cout << "Identifier of the " << max_min << " language: " 
+              << languages[min_max_index].getLanguageId() << std::endl;
     
     delete[] languages;
     return 0;
