fe09d9a0a1c6c053fd27b779571c1c18e236b6b2
Miguel <miguevrgo@gmail.com>
2023-05-18 18:51:47 +0200

    Change doConfig -> runZipProject -> Automatically create Zip with project name

--- Scripts/doConfig.sh
+++ Scripts/doConfig.sh
@@ -476,7 +476,7 @@ SECS_WAIT=15
 # Executes the script in interactive or batch mode
 DO_INTERACTIVE=YES
 # Forces the name of the ZIP. If left empty, the zip uses the name of the root foder
-ZIP_NAME=PRACTICAMP
+ZIP_NAME=$PROJECT_NAME
 if [ "$1" == "-help" ] || [ "$1" == "-h" ]
 then
 	viewHelp "$HELPTEXT"
